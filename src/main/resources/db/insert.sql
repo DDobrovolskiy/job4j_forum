b92688fce9ae08dd3b6fd9f16022ee05174ebaba
DmitriyDobrovolskiy <dda.90@mail.ru>
2021-09-19 00:09:03 +0500

    0. Spring Liquibase [#302330]

---
+++ src/main/resources/db/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO posts (name, description) VALUES
+('Test','Test description');
+
+INSERT INTO roles (title) VALUES
+('USER');
+
+INSERT INTO users (username, password, role_id) VALUES
+('user', 'password', 1);
